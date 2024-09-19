c8bf930ad2a3f7c4639767ea1d8776cd39d9494d
cyz608 <37165535+cyz608@users.noreply.github.com>
2024-09-19 23:22:39 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+﻿// 平滑滚动到指定位置
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
